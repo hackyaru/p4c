adb7a0cd8433d39b57011eaf45f54eca48eb9355
hackyaru <73215779+hackyaru@users.noreply.github.com>
2022-10-09 14:32:46 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <tilte>로그인</title>
-</head>
-
 <body>
   로그인
 </body>
