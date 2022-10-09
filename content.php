151f846df2c20f520ea4223931fd50cd2195c5fb
hackyaru <73215779+hackyaru@users.noreply.github.com>
2022-10-09 23:59:09 +0900

    Add files via upload

--- content.php
+++ content.php
@@ -1,3 +1,35 @@
 <?php
+	$conn = mysqli_connect("localhost","root","1234","page");
+	$query = "select * from ".$_GET['board']." where no = ".$_POST['no'].";";
+	$result =mysqli_query($conn, $query);
+	$row = mysqli_fetch_array($result);
+	if(!isset($_COOKIE['view'])) {
+		$query="update ".$_GET['board']." SET view=".(string)($row['view']+1)." WHERE no=".$_POST['no'].";";
+		setcookie('id',$_POST['id'],time() + 3600, "/");
+	}
+		echo "
+<html>
+<head>
+	<title>".$row['title']."</title>
+</head>
+<body>
 	
-?>
+	
+	<p>
+	제목: ".$row['title']."
+	</p>
+	<p>
+	".$row['content']."
+	</p>
+	<p>
+	
+	<form method='post' action='writesave.php?board=".$_GET['board']."'>
+	<input type='text' name='content'>
+	
+		<input type='submit'
+			   value='댓글쓰기'
+			   />
+	</form>
+</body>
+</html>"
+	?>
--- writesave.php
+++ writesave.php
@@ -1,3 +1,7 @@
 <?php
-	
+	$conn = mysqli_connect("localhost","root","1234","page");
+	$query = "insert into ".$_GET['board']."(title, content, id, star, view) values ('".$_POST['title']."', '".$_POST['content']."', '".$_COOKIE['id'].", 0, 0);";
+	$result =mysqli_query($conn, $query);
+	echo "<script>alert('게시글 작성이 완료되었습니다.');</script>";
+		echo "<script>location.href='".$_GET['board'].".php';</script>";
 ?>
